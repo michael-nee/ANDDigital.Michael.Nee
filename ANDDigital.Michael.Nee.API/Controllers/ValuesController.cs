3c06757efca5df4c32c1b803d70c7eefdf595d84
Michael Nee <university@michael-nee.co.uk>
2018-10-24 13:32:42 +0100

    Tests to Ensure WebAPI and Host Server are working.

---
+++ ANDDigital.Michael.Nee.API/Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ANDDigital.Michael.Nee.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public ValuesController()
+        {
+
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return Ok(new string[] { "value1", "value2" });
+        }
+    }
+}
